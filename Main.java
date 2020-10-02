6b5b1eb8109002c1fea5400c6e87e7d9cb4b436a
pajid9763 <pajid9763@wrdsb.ca>
2020-10-02 15:17:46 +0000

    finished while loops

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * while loops
+ * @author Darian
  */
 public class Main {
 
@@ -9,7 +10,15 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+  Scanner input = new Scanner(System.in);
+
+  System.out.println("Beginning to count!");
+  int count = 1; 
+  while (count <= 5){
+    System.out.println(count);
+    //count = count + 1;
+  }
+  System.out.println("That's all for now");
+
   }
 }
